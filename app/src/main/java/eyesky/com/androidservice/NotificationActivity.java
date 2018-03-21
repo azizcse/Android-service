1a61f60ab06e2dbf0ff5ef9dd9ee561002bbeb29
azizul <azizul@w3engineers.com>
2018-03-21 19:08:22 +0600

    Bind service

---
+++ app/src/main/java/eyesky/com/androidservice/NotificationActivity.java
@@ -0,0 +1,24 @@
+package eyesky.com.androidservice;
+
+/*
+*  ****************************************************************************
+*  * Created by : Md. Azizul Islam on 3/21/2018 at 6:37 PM.
+*  * Email : azizul@w3engineers.com
+*  * 
+*  * Last edited by : Md. Azizul Islam on 3/21/2018.
+*  * 
+*  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>  
+*  ****************************************************************************
+*/
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NotificationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
